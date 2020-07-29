63a1e74e1920b11c364464556a52dec551456bb7
Luiz Xavier <luiz.xavier.k@gmail.com>
2020-07-29 14:37:27 -0300

    first commit project

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Menu from './components/Menu'
+import dadosInicias from './data/dados_iniciais.json'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosInicias.categorias[0].videos[0].titulo}
+        url={dadosInicias.categorias[0].videos[0].url}
+        videoDescription={"?"}
+      
+      />
+      <Carousel
+        ignoreFirstVideo
+        category={dadosInicias.categorias[0]}
+      />
+      <Carousel
+        category={dadosInicias.categorias[1]}
+      />
+      <Carousel
+        category={dadosInicias.categorias[2]}
+      />
+      <Carousel
+        category={dadosInicias.categorias[3]}
+      />
+            <Carousel
+        category={dadosInicias.categorias[4]}
+      />
+      <Footer/>
+    </div>
+  )
+}
+
+export default App;
