b4516436151c101b2f3afe16afa38295d16b3f1a
Luiz Xavier <luiz.xavier.k@gmail.com>
2020-07-30 19:26:39 -0300

    rapidao commit project

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosInicias from './data/dados_iniciais.json'
-import BannerMain from './components/BannerMain'
-import Carousel from './components/Carousel'
-import Footer from './components/Footer';
+import Menu from '../../components/Menu'
+import dadosInicias from '../../data/dados_iniciais.json'
+import BannerMain from '../../components/BannerMain'
+import Carousel from '../../components/Carousel'
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
     <div style={{ background: "#141414" }}>
       <Menu />
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosInicias.categorias[0].videos[0].titulo}
         url={dadosInicias.categorias[0].videos[0].url}
-        videoDescription={"Traduzido do inglês-O futurismo foi um movimento artístico do início do século XX, que incluía pintura, escultura, poesia, teatro, música, arquitetura, cinema e gastronomia. Filippo Tommaso Marinetti iniciou o movimento com seu Manifesto of Futurism, publicado em fevereiro de 1909."}
+        videoDescription={"Traduzido do inglês o futurismo foi um movimento artístico do início do século XX, que incluía pintura, escultura, poesia, teatro, música, arquitetura, cinema e gastronomia. Filippo Tommaso Marinetti iniciou o movimento com seu Manifesto of Futurism, publicado em fevereiro de 1909."}
       
       />
       <Carousel
@@ -37,4 +37,4 @@ function App() {
   )
 }
 
-export default App;
+export default Home;
