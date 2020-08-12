1232d22f02108c974d2c1c269b829770f6a2db52
Luiz Xavier <luiz.xavier.k@gmail.com>
2020-08-11 22:20:59 -0300

    five commit project

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,72 @@
-import React, { useEffect } from 'react';
-import Menu from '../../components/Menu';
-import dadosInicias from '../../data/dados_iniciais.json';
+import React, { useState, useEffect } from 'react';
+// import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
+import PageDefault from '../../components/PageDefault';
 import categoriasRepository from '../../repositories/categorias';
 
 function Home() {
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+
   useEffect(() => {
+    // http://localhost:8080/categorias?_embed=videos
     categoriasRepository.getAllWithVideos()
       .then((categoriasComVideos) => {
-        // eslint-disable-next-line no-console
-        console.log(categoriasComVideos);
+        console.log(categoriasComVideos[0].videos[0]);
+        setDadosIniciais(categoriasComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
       });
-  });
-  // http://localhost:8080/categorias?_embed=videos
+  }, []);
 
   return (
-    <div style={{ background: '#141414' }}>
-      <Menu />
+    <PageDefault paddingAll={0}>
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
 
-      <BannerMain
-        videoTitle={dadosInicias.categorias[0].videos[0].titulo}
-        url={dadosInicias.categorias[0].videos[0].url}
-        videoDescription={"Cyberpunk (de Cyber(netic) + punk) é um subgênero alternativo de ficção científica, conhecido por seu enfoque de 'Alta tecnologia e baixa qualidade de vida' (High tech, Low life) e toma seu nome da combinação de cibernética e punk alternativo. Mescla ciência avançada, como as tecnologias de informação e a cibernética junto com algum grau de desintegração ou mudança radical no sistema civil vigente. O movimento que incluí pintura, escultura, poesia, teatro, música, arquitetura, cinema e gastronomia."}
-      />
-      <Carousel
-        ignoreFirstVideo
-        category={dadosInicias.categorias[0]}
-      />
-      <Carousel
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription="Cyberpunk (de Cyber(netic) + punk) é um subgênero alternativo de ficção científica, conhecido por seu enfoque de 'Alta tecnologia e baixa qualidade de vida' (High tech, Low life) e toma seu nome da combinação de cibernética e punk alternativo. Mescla ciência avançada, como as tecnologias de informação e a cibernética junto com algum grau de desintegração ou mudança radical no sistema civil vigente. O movimento que incluí pintura, escultura, poesia, teatro, música, arquitetura, cinema e gastronomia."
+              />
+
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+
+            </div>
+          );
+        }
+
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
+
+      {/*       {dadosIniciais.length >= 1 && (
+      <>
+        <BannerMain
+          videoTitle={dadosIniciais[0].videos[0].titulo}
+          url={dadosIniciais[0].videos[0].url}
+          videoDescription="Cyberpunk (de Cyber(netic) + punk) é um subgênero alternativo de ficção científica, conhecido por seu enfoque de 'Alta tecnologia e baixa qualidade de vida' (High tech, Low life) e toma seu nome da combinação de cibernética e punk alternativo. Mescla ciência avançada, como as tecnologias de informação e a cibernética junto com algum grau de desintegração ou mudança radical no sistema civil vigente. O movimento que incluí pintura, escultura, poesia, teatro, música, arquitetura, cinema e gastronomia."
+        />
+
+        <Carousel
+          ignoreFirstVideo
+          category={dadosIniciais[0]}
+        />
+      </>
+      )}
+
+      { <Carousel
         category={dadosInicias.categorias[1]}
       />
       <Carousel
@@ -40,9 +77,9 @@ function Home() {
       />
       <Carousel
         category={dadosInicias.categorias[4]}
-      />
-      <Footer />
-    </div>
+      /> } */}
+
+    </PageDefault>
   );
 }
 
